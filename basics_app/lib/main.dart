4fa0943395bc314d029b2d5f09a43aaf3bccef0c
SripaliManimekala <sripalimanimekala@gmail.com>
2024-04-22 11:28:00 +0530

    quiz app start screen opacity widget and icon

--- basics_app/lib/main.dart
+++ basics_app/lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:basics_app/gradient_container.dart';
+import 'package:basics_app/start_screen.dart';
+
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: GradientContainer(),
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: <Color>[
+                  Color.fromARGB(255, 64, 5, 109),
+                  Color.fromARGB(255, 87, 15, 143)
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+            ),
+            child: const StartScreen()),
       ),
     ),
   );
---
+++ basics_app/lib/start_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class StartScreen extends StatelessWidget {
+
+  const StartScreen ({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+
+      return Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,//will only take each widgets min size: image 300px
+            children: [
+              Opacity(
+                opacity: 0.5,
+                child: Image.asset(
+                  'assets/images/quiz-logo.png',
+                  width: 300,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 50, bottom: 30),
+                child: Text(
+                'Learn Flutter the fun way!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24
+                ),
+              ),),
+              OutlinedButton.icon(
+                onPressed: () {print('button pressed');},
+                style: OutlinedButton.styleFrom(
+                  // backgroundColor:const Color.fromARGB(255, 64, 5, 109),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                icon: const Icon(Icons.arrow_right_alt),
+                label: const Text('Start Quiz')),
+            ],
+          ),
+        );
+  }
+}
