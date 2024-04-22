b27f900483aa66b02582d358f4d135f70ecdd72a
SripaliManimekala <sripalimanimekala@gmail.com>
2024-04-22 16:56:16 +0530

    output questions from list of questions

--- basics_app/lib/questions_screen.dart
+++ basics_app/lib/questions_screen.dart
@@ -12,27 +12,42 @@ class QuestionsScreen extends StatefulWidget {
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen> {
+
+  var currentQuestionIndex = 0;
+
+  void answerQuestion() {
+    // currentQuestionIndex += 1;
+    setState(() {
+      currentQuestionIndex++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
 
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
 
     return SizedBox(
       width: double.infinity, //use as much width as u can
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment:  CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(height: 30),
-          ...currentQuestion.answers.map((answer) { 
-            return AnswerButton(answerText: answer, onTap: () {});
-            }),//pull out list of buttons to individual buttons(..spread operator)
-        ],
+            const SizedBox(height: 30),
+            ...currentQuestion.getShuffledAnswers().map((answer) { 
+              return AnswerButton(answerText: answer, onTap: answerQuestion);
+              }),//pull out list of buttons to individual buttons(..spread operator)
+          ],
+        ),
       ),
     );
   }
--- basics_app/lib/quiz.dart
+++ basics_app/lib/quiz.dart
@@ -1,7 +1,7 @@
 import 'package:basics_app/questions_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:basics_app/start_screen.dart';
-import 'package:flutter/widgets.dart';
+
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
