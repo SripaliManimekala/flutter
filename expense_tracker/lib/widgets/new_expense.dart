34bd3265abdca37eb15aadbc4720e716df5bad89
sripali <sripali@codevus.com>
2024-04-26 09:43:58 +0530

    add buttion to save the title value

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
-
   const NewExpense({super.key});
 
   @override
@@ -19,21 +18,27 @@ class _NewExpenseState extends State<NewExpense> {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
+    return Padding(
       padding: const EdgeInsets.all(20),
       child: Column(
         children: [
           TextField(
             onChanged: _saveTitleInput,
             maxLength: 50,
-            decoration: const InputDecoration(
-              label: Text('Title')
-            ),
+            decoration: const InputDecoration(label: Text('Title')),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  print(_enteredTitle);
+                },
+                child: const Text('Save Expense'),
+              )
+            ],
           ),
         ],
       ),
-      );
-
-      
+    );
   }
 }
