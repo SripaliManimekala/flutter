d65a7843c0b4ae22d11425dd110b4430e5f99a5a
sripali <sripali@codevus.com>
2024-04-29 12:09:17 +0530

    use only portrait mode

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,29 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+// import 'package:flutter/services.dart';
 
 //create color scheme
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 128, 191, 239));
-var kDarkColorScheme =
-    ColorScheme.fromSeed(
-      brightness: Brightness.dark,
-      seedColor: Color.fromARGB(255, 5, 99, 125));
+var kDarkColorScheme = ColorScheme.fromSeed(
+    brightness: Brightness.dark, seedColor: Color.fromARGB(255, 5, 99, 125));
 
 void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) =>
   runApp(
     MaterialApp(
       darkTheme: ThemeData.dark().copyWith(
         colorScheme: kDarkColorScheme,
-      cardTheme: const CardTheme().copyWith(
-        color: kDarkColorScheme.secondaryContainer,
-        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      ),
-      elevatedButtonTheme: ElevatedButtonThemeData(
-        style: ElevatedButton.styleFrom(
-        backgroundColor: kDarkColorScheme.primaryContainer,
-        foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         ),
-      ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
       ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
@@ -52,4 +55,5 @@ void main() {
       home: const Expenses(),
     ),
   );
+  // );
 }
