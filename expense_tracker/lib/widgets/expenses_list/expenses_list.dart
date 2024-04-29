ea1ddad7ee9370fea5fe83e82a3cf7a154bbbde4
sripali <sripali@codevus.com>
2024-04-29 09:29:36 +0530

    set bakground color for dismissible item using themes and set its margin using themes

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -3,7 +3,8 @@ import 'package:expense_tracker/models/expense.dart';
 import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses, required this.onRemoveExpense});
+  const ExpensesList(
+      {super.key, required this.expenses, required this.onRemoveExpense});
 
   final List<Expense> expenses;
   final void Function(Expense expense) onRemoveExpense;
@@ -11,13 +12,16 @@ class ExpensesList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: expenses.length, 
+        itemCount: expenses.length,
         itemBuilder: (ctx, index) => Dismissible(
-          background: Container(color: Color.fromARGB(255, 236, 183, 179),),
-          onDismissed: (direction) {
-            onRemoveExpense(expenses[index]);
-          },
-          key: ValueKey(expenses[index]), 
-          child: ExpenseItem(expenses[index])));
+            background: Container(
+              color: Theme.of(context).colorScheme.error,
+              margin: Theme.of(context).cardTheme.margin,
+            ),
+            onDismissed: (direction) {
+              onRemoveExpense(expenses[index]);
+            },
+            key: ValueKey(expenses[index]),
+            child: ExpenseItem(expenses[index])));
   }
 }
