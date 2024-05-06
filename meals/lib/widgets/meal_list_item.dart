4a5db0529471802a32d14892d00606fdd08e4d1a
sripali <sripali@codevus.com>
2024-05-06 09:15:01 +0530

    meal list meal item trait - to output time and complexity

--- meals/lib/widgets/meal_list_item.dart
+++ meals/lib/widgets/meal_list_item.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item_trait.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class MealListItem extends StatelessWidget {
   const MealListItem({super.key, required this.meal});
 
+  String get complexityText {//get complexity  as Complexity
+    return meal.complexity.name[0].toUpperCase() + meal.complexity.name.substring(1);
+  }
+
   final Meal meal;
   @override
   Widget build(BuildContext context) {
     return Card(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      clipBehavior: Clip
+          .hardEdge, //enforce the shape by cutting out any content that go outside the shape
+      elevation: 2, //give some 3D effect to the card
       child: InkWell(
         onTap: () {},
         child: Stack(
           children: [
             FadeInImage(
               placeholder: MemoryImage(kTransparentImage),
-              image: NetworkImage(meal.imageUrl)),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover, //image is cutoff and fixed
+              height: 200,
+              width: double.infinity,
+            ),
             Positioned(
-              bottom: 20,
-              left: 20,
-              right: 20,
+              bottom: 0,
+              left: 0,
+              right: 0,
               child: Container(
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
@@ -28,26 +42,32 @@ class MealListItem extends StatelessWidget {
                 padding: EdgeInsets.all(5),
                 child: Column(
                   children: [
-                  Text(
-                  meal.title,
-                  maxLines: 2,
-                  textAlign: TextAlign.center,
-                  softWrap: true,
-                  overflow: TextOverflow.ellipsis,//example text ...
-                  style: const TextStyle(
-                    color: Color.fromARGB(255, 255, 255, 255),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
+                    Text(
+                      meal.title,
+                      maxLines: 2,
+                      textAlign: TextAlign.center,
+                      softWrap: true,
+                      overflow: TextOverflow.ellipsis, //example text ...
+                      style: const TextStyle(
+                        color: Color.fromARGB(255, 255, 255, 255),
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: 3),
-                  const Row(
-                    children: [
-                      Text('test1'),
-                      const SizedBox(width: 10),
-                      Text('test1'),
-                    ],
-                  ),
+                    const SizedBox(height: 3),
+                    Row(
+                      children: [
+                        MealItemTrait(
+                          icon: Icons.schedule,
+                          label: '${meal.duration} min',
+                        ),
+                        const SizedBox(width: 12,),
+                        MealItemTrait(
+                          icon: Icons.work,
+                          label: '${complexityText}',// meal.complexity.name
+                        )
+                      ],
+                    ),
                   ],
                 ),
               ),
