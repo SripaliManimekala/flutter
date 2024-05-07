38b41481cd8fd2b149580d85ca91333ad06c1128
sripali <sripali@codevus.com>
2024-05-07 09:43:15 +0530

    get data from filter screen to tab screen and filter dummy meals, then pass filtered meals to category and show filtered based on category

--- meals/lib/screens/tabs.dart
+++ meals/lib/screens/tabs.dart
@@ -1,15 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
 import 'package:meals/models/meal.dart';
 import 'package:meals/widgets/main_drawer.dart';
 
-class TabsScreen extends StatefulWidget {
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false,
+};
 
+class TabsScreen extends StatefulWidget {
   const TabsScreen({super.key});
 
-@override
+  @override
   State<TabsScreen> createState() {
     return _TabsScreenState();
   }
@@ -18,6 +25,8 @@ class TabsScreen extends StatefulWidget {
 class _TabsScreenState extends State<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favouriteMeals = [];
+  Map<Filter, bool> _selectedFilters =
+      kInitialFilters; //variable to store selected filters
 
   void _showInfoMessage(String message) {
     ScaffoldMessenger.of(context).clearSnackBars();
@@ -30,7 +39,7 @@ class _TabsScreenState extends State<TabsScreen> {
   void _toggleMealFavouriteStatus(Meal meal) {
     final isExisting = _favouriteMeals.contains(meal);
 
-    if(isExisting){
+    if (isExisting) {
       setState(() {
         _favouriteMeals.remove(meal);
         _showInfoMessage('Meal is no longer a favourite!');
@@ -49,48 +58,76 @@ class _TabsScreenState extends State<TabsScreen> {
     });
   }
 
-  void _setScreen(String identifier)  async{
-     Navigator.of(context).pop();//cloase the drawer and its tabs screen
-    if(identifier=='filters') {
-      final result = await Navigator.of(context).push<Map<Filter,bool>>(
+  void _setScreen(String identifier) async {
+    Navigator.of(context).pop(); //cloase the drawer and its tabs screen
+    if (identifier == 'filters') {
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
-        //get the filters as a future..after user interaction with the filter screen and click back button
-        builder: (ctx) => const FiltersScreen()
-      ),
+            //get the filters as a future..after user interaction with the filter screen and click back button
+            builder: (ctx) => const FiltersScreen()),
       );
-      print(result);
-    } 
+      setState(() {
+        _selectedFilters = result ??
+            kInitialFilters; //if result null,_selectedFilters will fallback to kInitialFilters
+      });
+    }
   }
 
-@override
+  @override
   Widget build(BuildContext context) {
-    
-    Widget activePage = CategoriesScreen(onToggleFavourite: _toggleMealFavouriteStatus,);
+    final availbleMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        //if filter is set and meal doesnt satisfy the filter
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        //if filter is set and meal doesnt satisfy the filter
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        //if filter is set and meal doesnt satisfy the filter
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        //if filter is set and meal doesnt satisfy the filter
+        return false;
+      }
+      return true; //if all filters are not set or meal satisfies them it returns true
+    }).toList();
+
+    ///we should pass filters to the category screen
+    Widget activePage = CategoriesScreen(
+      onToggleFavourite: _toggleMealFavouriteStatus,
+      availableMeals: availbleMeals,
+    );
     var activePageTitle = 'Categories';
 
-    if(_selectedPageIndex==1) {
+    if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
         meals: _favouriteMeals,
         onToggleFavourite: _toggleMealFavouriteStatus,
       );
-      activePageTitle= 'Your Favorites';
+      activePageTitle = 'Your Favorites';
     }
 
     return Scaffold(
       appBar: AppBar(
         title: Text(activePageTitle),
       ),
-      drawer: MainDrawer(onSelectScreen: _setScreen,),
+      drawer: MainDrawer(
+        onSelectScreen: _setScreen,
+      ),
       body: activePage,
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
-        currentIndex: _selectedPageIndex,//controll wich tab should be highlighted
+        currentIndex:
+            _selectedPageIndex, //controll wich tab should be highlighted
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.set_meal),
             label: 'Categories',
           ),
-                    BottomNavigationBarItem(
+          BottomNavigationBarItem(
             icon: Icon(Icons.star),
             label: 'Favourites',
           ),
