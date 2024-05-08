56737b8557216ccd490a95279c19d291ca8be096
sripali <sripali@codevus.com>
2024-05-08 15:42:08 +0530

    models and data

--- shopping_list/lib/models/category.dart
+++ shopping_list/lib/models/category.dart
@@ -1,11 +1,26 @@
+import 'package:flutter/material.dart';
+
 enum Categories {
   vegetables,
   fruit,
   meat,
   dairy,
-  carbs
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other
 }
 
 class Category {
+
+   const Category(
+    this.title,
+    this.color,
+  );
+  
+  final String title;
+  final Color color;
 
 }
